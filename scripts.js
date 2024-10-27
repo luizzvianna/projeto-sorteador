f1faba688d7699171c2e887fe77d1ebf9634bcea
luizzvianna <luizvianna.iin@gmai.com>
2024-10-27 14:09:22 -0400

    adicionado alert no projeto

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 
 
-function butonSelected(){
+function butonSelected() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
-   
-    const sort =  Math.floor(Math.random() * (max - min + 1)) + min;
 
-    document.getElementById("resultado").innerHTML=sort;
-    
-    alert (result)
-    
+    if (min >= max) {
+        alert("OPA! VALOR MINIMO NÃO PODE SER MAIOR QUE O VALOR MAXIMO!")
+    }
+
+    const sort = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    document.getElementById("resultado").innerHTML = sort;
+
+    alert(result)
 
 }
 
